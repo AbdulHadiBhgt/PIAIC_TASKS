4827ece1b298ea2be23f54b8a0746ef098e74a3d
Abdul Hadi Bhagat <80905227+AbdulHadiBhgt@users.noreply.github.com>
2024-03-20 14:16:04 +0500

    simple calc with inquire and eval

---
+++ index.js
@@ -0,0 +1,9 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter 1st No :", type: "number", name: "firstNo" },
+    { message: "Enter 2nd No :", type: "number", name: "secNo" },
+    { message: "Select Operator:", type: "list", name: "operator", choices: ["+", "-", "*", "/"] },
+]);
+//console.log(answer);
+let sum = eval(answer.firstNo + answer.operator + answer.secNo);
+console.log("Your answer is : ", sum);
